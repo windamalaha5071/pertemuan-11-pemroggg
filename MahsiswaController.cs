84895e3933540adfede6a6ca532ff20e758a8eeb
windamalaha5071 <146574372+windamalaha5071@users.noreply.github.com>
2023-11-29 16:18:00 +0700

    Add files via upload

---
+++ MahsiswaController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PerpustakaanAppMVC;
+using PerpustakaanAppMVC.Model.Repository;
+using PerpustakaanAppMVC.Model.Context;
+using PerpustakaanAppMVC.Model.Entity;
+
+namespace PerpustakaanAppMVC.Model
+{
+    public class MahsiswaController
+    {
+        private MahasiswaRepository _repository;
+
+        public int Create(Mahasiswa mhs)
+        {
+            int result = 0;
+            // cek npm yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Npm))
+            {
+                MessageBox.Show("NPM harus diisi !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            // cek nama yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Nama))
+            {
+                MessageBox.Show("Nama harus diisi !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            // cek angkatan yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Angkatan))
+            {
+                MessageBox.Show("Angkatan harus diisi !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            // membuat objek context menggunakan blok using
+            using (DbContext context = new DbContext())
+            {
+                // membuat objek class repository
+                _repository = new MahasiswaRepository(context);
+                // panggil method Create class repository untuk menambahkan data
+                result = _repository.Create(mhs);
+            }
+            if (result > 0)
+            {
+                MessageBox.Show("Data mahasiswa berhasil disimpan !", "Informasi",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+                MessageBox.Show("Data mahasiswa gagal disimpan !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            return result;
+        }
+
+        public int Update(Mahasiswa mhs)
+        {
+            int result = 0;
+            // cek npm yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Npm))
+            {
+                MessageBox.Show("NPM harus diisi !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            // cek nama yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Nama))
+            {
+                MessageBox.Show("Nama harus diisi !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+            // cek angkatan yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Angkatan))
+            {
+                MessageBox.Show("Angkatan harus diisi !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+
+            using (DbContext context = new DbContext())
+            {
+                _repository = new MahasiswaRepository(context);
+                result = _repository.Update(mhs);
+            }
+
+            if (result > 0)
+            {
+                MessageBox.Show("Data mahasiswa berhasil diupdate !", "Informasi",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Data mahasiswa gagal diupdate !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+            return result;
+        }
+
+        public int Delete(Mahasiswa mhs)
+        {
+            int result = 0;
+            // cek npm yang diinputkan tidak boleh kosong
+            if (string.IsNullOrEmpty(mhs.Npm))
+            {
+                MessageBox.Show("NPM harus diisi !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return 0;
+            }
+
+            using (DbContext context = new DbContext())
+            {
+                _repository = new MahasiswaRepository(context);
+                result = _repository.Delete(mhs);
+            }
+
+            if (result > 0)
+            {
+                MessageBox.Show("Data mahasiswa berhasil dihapus !", "Informasi",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Data mahasiswa gagal dihapus !!!", "Peringatan",
+                MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+            return result;
+        }
+
+        public List<Mahasiswa> ReadByNama(string nama)
+        {
+            // membuat objek collection
+            List<Mahasiswa> list = new List<Mahasiswa>();
+            // membuat objek context menggunakan blok using
+            using (DbContext context = new DbContext())
+            {
+                // membuat objek dari class repository
+                _repository = new MahasiswaRepository(context);
+                // panggil method GetByNama yang ada di dalam class repository
+                list = _repository.ReadByNama(nama);
+            }
+            return list;
+        }
+        public List<Mahasiswa> ReadAll()
+        {
+            // membuat objek collection
+            List<Mahasiswa> list = new List<Mahasiswa>();
+            // membuat objek context menggunakan blok using
+            using (DbContext context = new DbContext())
+            {
+                // membuat objek dari class repository
+                _repository = new MahasiswaRepository(context);
+                // panggil method GetAll yang ada di dalam class repository
+                list = _repository.ReadAll();
+            }
+            return list;
+        }
+    }
+}
